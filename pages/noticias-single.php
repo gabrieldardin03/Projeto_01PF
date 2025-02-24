83d2e845750cdc3ce61a654b49a4fae5e524d336
gabrieldardin03 <dardingabriel@gmail.com>
2025-02-23 22:07:07 -0300

    .

--- pages/noticias-single.php
+++ pages/noticias-single.php
@@ -1,46 +1,48 @@
 <?php 
-//obtendo a categoria
+// Obtendo a categoria
+$url = explode('/', $_GET['url']);
 
-$url = explode('/',$_GET['url']);
+// Verifica se a categoria existe
 $verificaCategoria = MySql::conectar()->prepare("SELECT * 
-                                                FROM ´tb_admin.categorias´
-                                                  WHERE slug = ?");
-
+                                                FROM `tb_admin.categorias`
+                                                WHERE slug = ?");
 $verificaCategoria->execute(array($url[1]));
-if($verificaCategoria->rowCount() == 0){
+
+if ($verificaCategoria->rowCount() == 0) {
     Painel::redirect(INCLUDE_PATH . 'noticias');
 }
 
 $categoriaInfo = $verificaCategoria->fetch();
+
+// Verifica se a notícia existe
 $post = MySql::conectar()->prepare("SELECT *        
-                                   FROM `tb_admin.noticias´    
+                                   FROM `tb_admin.noticias`    
                                    WHERE slug = ?
-                                   AND categoria_id =?");
+                                   AND categoria_id = ?");
 $post->execute(array($url[2], $categoriaInfo['id']));
-if($post->rowCount() == 0){
-    Painel::redirect(INCLUDE_PATH .'noticias');
-}                
-//a minha noticia existe
 
+if ($post->rowCount() == 0) {
+    Painel::redirect(INCLUDE_PATH . 'noticias');
+}
+
+// A notícia existe
 $post = $post->fetch();
-
 ?>
 
 <section class="noticia-single">
-<div class="center">
-    <div class="text-center">
-        <header>
-            <h1><?php echo $post['titulo'];?></h1>
-        </header>
-        <article>
-            <img src="<?php echo INCLUDE_PATH_PAINEL;?>uploads/<?php echo $post['capa'];?>">
-            <?php echo $post['conteudo'];?>
-            <div class="box-single-conteudo btnVoltar">
-                <a href="<?php echo INCLUDE_PATH;?> noticias">Voltar</a>
-            </div>
-            <div class="clear"></div><!--clear-->
-        </article>
+    <div class="center">
+        <div class="text-center">
+            <header>
+                <h1><?php echo $post['titulo']; ?></h1>
+            </header>
+            <article>
+                <img src="<?php echo INCLUDE_PATH_PAINEL; ?>uploads/<?php echo $post['capa']; ?>">
+                <?php echo $post['conteudo']; ?>
+                <div class="box-single-conteudo btnVoltar">
+                    <a href="<?php echo INCLUDE_PATH; ?>noticias">Voltar</a>
+                </div>
+                <div class="clear"></div><!--clear-->
+            </article>
+        </div>
     </div>
-</div>
-
 </section>
